9c208d3b413a55ffa3b1dae5a8ea833a04884bc9
Stephen Fitz <sf3125@zeus.local>
2020-07-10 11:51:35 +0900

    a

---
+++ project/keio2020adp_XXXXXXXX_project/run_script.py
@@ -0,0 +1,20 @@
+##
+from sonar.sonar_data import *
+from sonar.sonar_model import *
+from sonar.sonar_trainer import *
+##
+
+##
+def main():
+
+    data = YOUR_CODE
+    model = YOUR_CODE
+    trainer = YOUR_CODE
+    costs, accuracies = YOUR_CODE
+    model.save_model()
+##
+
+##
+if __name__ == "__main__":
+    main()
+##
